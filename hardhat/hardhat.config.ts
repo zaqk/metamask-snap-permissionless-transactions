2ace605e288d4f02c50b6220a7b5d4be7b484f8f
zaqk <10659278+zaqk@users.noreply.github.com>
2022-04-17 00:10:31 -0400

    clean up

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -5,28 +5,12 @@ import * as dotenv from "dotenv";
 dotenv.config()
 
 const config: HardhatUserConfig =  {
-  solidity: {
-    version: '0.8.12',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
   networks: {
     hardhat: {
       accounts: {
         mnemonic: process.env.MNEMONIC,
       }
     },
-  },
-  paths: {
-    sources: "src",
-    deploy: 'deploy',
-  },
-  namedAccounts: {
-    deployer: 0,
   }
 };
 
